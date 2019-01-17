2e5ff96834549fd917ff6b7272c4c0af94159e1e
Moonyll <s.pouwel@laposte.net>
2019-01-17 15:50:57 +0100

    Jqqq4

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#ha').click(function () {
+        var hauteur = $('#rect').height();
+        if (hauteur <= 100)
+            $('#rect').css('height', hauteur + 10);
+        else
+            hauteur = 10;
+    });
+    $('#cl').click(function () { $('#rect').css('border', '3px solid green'); });
+    $('#icl').click(function () { $('#rect').css('border', '3px solid blue'); });
+    $('#hid').click(function () { $('#rect').hide() });
+    $('#sho').click(function () { $('#rect').show() });
+}
+);
