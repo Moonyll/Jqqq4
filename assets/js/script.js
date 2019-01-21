02db48db6dfb12b0a8a831905e879f22fdd396ec
Moonyll <45941071+Moonyll@users.noreply.github.com>
2019-01-21 12:22:51 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,30 @@
     $('#sho').click(function () { $('#rect').show() });
 }
 );
+Amélioration :
+// Fonction Event sur tous les boutons
+$('.button').click((event) => {
+    // Si c'est le premier bouton qui a été cliqué
+    if ($(event.target).attr('id') == 'btn1') {
+        var height = $('#rectangle').height(); // on récupère la hauteur actuelle
+        if (height + 10 > 100) {
+            // Si hauteur + 10 supérieure à 100, on réinitialise.
+            $('#rectangle').height(10);
+        } else {
+            // Sinon, on additionne.
+            $('#rectangle').height(height + 10);
+        }
+        // Si c'est le deuxième bouton qui a été cliqué
+    } else if ($(event.target).attr('id') == 'btn2') {
+        $('#rectangle').css('background', 'green'); // On met le fond en vert
+        // Si c'est le troisième bouton qui a été cliqué
+    } else if ($(event.target).attr('id') == 'btn3') {
+        $('#rectangle').css('background', ''); // On remet le fond à zéro
+        // Si c'est le quatrième bouton qui a été cliqué
+    } else if ($(event.target).attr('id') == 'btn4') {
+        $('#rectangle').hide(); // on cache la div
+        // Si c'est le cinquième bouton qui a été cliqué
+    } else if ($(event.target).attr('id') == 'btn5') {
+        $('#rectangle').show(); // on montre la div
+    }
+});
